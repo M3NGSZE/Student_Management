e020e0ed460636651af1b7f43aecad3ae23999f6
M3NGSZE <mengse.thun13@gmail.com>
2025-09-19 16:55:57 +0700

    create student entity

---
+++ src/main/java/com/sze/studentmanagement/model/entity/Enrollment.java
@@ -0,0 +1,4 @@
+package com.sze.studentmanagement.model.entity;
+
+public class Enrollment {
+}
---
+++ src/main/java/com/sze/studentmanagement/model/entity/Student.java
@@ -0,0 +1,19 @@
+package com.sze.studentmanagement.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Table(name = "student_tb")
+@Data
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+}
